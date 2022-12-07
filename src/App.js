6ad6be362e9e0fcb4ad1351de91d0325328db311
Sru03 <94092731+Sru03@users.noreply.github.com>
2022-12-07 20:12:19 +1300

    modified

--- src/App.js
+++ src/App.js
@@ -3,21 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1> HELLO SRUSHTI Let's go and build the instagram app </h1>
+      <div className="app__header">
+        <img
+        className="app__headerImage"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027"
+
+        alt=" "
+        
+        />
+          
+      </div>
     </div>
   );
 }
