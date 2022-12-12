7804e20c37a3c258341bbf32837cff723b33dd64
Sru03 <94092731+Sru03@users.noreply.github.com>
2022-12-12 22:16:30 +1300

    home page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Post from './Post';
 
 function App() {
   return (
     <div className="app">
-      <h1> HELLO SRUSHTI Let's go and build the instagram app </h1>
+  
       <div className="app__header">
         <img
         className="app__headerImage"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027"
+        width="130"
+        height="50"
 
         alt=" "
-        
         />
-          
+         
       </div>
+
+
+      {/* <h1> Header  </h1> */}
+           {/* <Post /> */}
+          {/* Posts */}
+          {/* Posts */}
+
+
+
     </div>
   );
 }
---
+++ src/Post.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <div>
+        <h3>Username</h3>
+
+        {/* header ->avatar + username*/}
+
+        {/* IMAGE */}
+
+        {/* username + caption */}
+
+    </div>
+  )
+}
+
+export default Post
