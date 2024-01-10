4914c10df81977fead96d503a8dc6a485d67e8a5
Srushti M.B <mbsrushti@gmail.com>
2024-01-10 20:38:38 +1300

    new changes restarted react app

---
+++ instagram-clone/src/Post.jsx
@@ -0,0 +1,36 @@
+// rfce ~ shortcut command that creates the template for you 
+
+import React from 'react';
+import './Post.css';
+import Avatar from "@material-ui/core/Avatar";
+
+
+function Post({username, caption, imageUrl}) {
+  return (
+    <div className = "post">
+
+      <div className = "post_header"> 
+      <Avatar
+        className = "post_avatar"
+        alt = {username}
+        src = "/static/images/avatar/1.jpg"
+
+      /> 
+      <h3>{username}</h3>
+      </div>
+
+        {/* header ->avatar + username*/}
+
+        <img  
+        className = "post_image" 
+        src = {imageUrl}></img>
+        {/* IMAGE */}
+        
+        <h4 className = "post_name">  <strong> {username} :  </strong> {caption}</h4>
+        {/* username + caption */}
+
+    </div>
+  )
+}
+
+export default Post
